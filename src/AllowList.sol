89e0c830530b22a4bf2a21fbfde143cb8a75926d
Rare Weasel <rareweasel@protonmail.com>
2024-01-12 17:36:46 -0600

    feat: add invariant tests

--- src/AllowList.sol
+++ src/AllowList.sol
@@ -7,12 +7,29 @@ contract AllowList {
 
     constructor(address _owner, address[] memory _allowList) {
         require(_owner != address(0), "invalid owner");
-        require(_allowList.length > 0, "invalid allowList length");
         owner = _owner;
         allowList = _allowList;
     }
 
     function getAllowList() public view returns (address[] memory) {
         return allowList;
+    }
+
+    function addItem(address _item) external returns (bool) {
+        require(msg.sender == owner, "only owner");
+        allowList.push(_item);
+        return true;
+    }
+
+    function removeItem(address _item) external returns (bool) {
+        require(msg.sender == owner, "only owner");
+        for (uint256 i = 0; i < allowList.length; i++) {
+            if (allowList[i] == _item) {
+                allowList[i] = allowList[allowList.length - 1];
+                allowList.pop();
+                return true;
+            }
+        }
+        return false;
     }
 }
---
+++ test/invariant/handlers/AllowListHandler.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import {CommonBase} from "forge-std/Base.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import {StdUtils} from "forge-std/StdUtils.sol";
+import {console2 as console} from "forge-std/console2.sol";
+import {AllowList} from "../../../src/AllowList.sol";
+
+contract AllowListHandler is CommonBase, StdCheats, StdUtils {
+    AllowList private allowList;
+
+    constructor(AllowList _allowList) {
+        allowList = _allowList;
+    }
+
+    function getAllowListLength() external view returns (uint256) {
+        return allowList.getAllowList().length;
+    }
+
+    function addItem(address _item) external returns (bool) {
+        hoax(allowList.owner());
+        return allowList.addItem(_item);
+    }
+
+    function addItem(address _sender, address _item) external returns (bool) {
+        hoax(_sender);
+        return allowList.addItem(_item);
+    }
+
+    function removeItem(address _item) external returns (bool) {
+        hoax(allowList.owner());
+        return allowList.removeItem(_item);
+    }
+
+    function removeItem(
+        address _sender,
+        address _item
+    ) external returns (bool) {
+        hoax(_sender);
+        return allowList.removeItem(_item);
+    }
+}
