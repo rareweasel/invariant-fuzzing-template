f1cab014dbe121d44def37f71412638e62c19c3a
Rare Weasel <89795397+rareweasel@users.noreply.github.com>
2024-01-16 15:48:31 -0600

    feat: add utils (#8)
    * feat: add utils
    
    * feat: fix ci
    
    * feat: fix ci
    
    * feat: fix ci
    
    * feat: fix ci
    
    * feat: fix ci
    
    * feat: fix ci
    
    * feat: fix ci
    
    * feat: fix ci

---
+++ test/mocks/MockToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import {ERC20PresetFixedSupply} from "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+
+contract MockToken is ERC20PresetFixedSupply {
+    constructor()
+        ERC20PresetFixedSupply(
+            "MockToken",
+            "MT",
+            900000000 * 10 ** 18,
+            msg.sender
+        )
+    {}
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
---
+++ test/utils/UintUtils.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import {StdUtils} from "forge-std/StdUtils.sol";
+
+abstract contract UintUtils is StdUtils {
+    function _uint256ToAddress(uint256 seed) internal view returns (address) {
+        return address(uint160(bound(seed, 1, type(uint160).max)));
+    }
+}
