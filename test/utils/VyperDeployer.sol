7de65f15cb678a23adf693deed67da49fbda9f59
Rare Weasel <89795397+rareweasel@users.noreply.github.com>
2024-04-19 17:31:32 -0600

    feat: support ape framework (#16)
    * feat: support ape framework
    
    * feat: fix ci issues

--- test/utils/VyperDeployer.sol
+++ test/utils/VyperDeployer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity 0.8.20;
 
 ///@notice This cheat codes interface is named _CheatCodes so you can use the CheatCodes interface in other testing files without errors
 interface _CheatCodes {
